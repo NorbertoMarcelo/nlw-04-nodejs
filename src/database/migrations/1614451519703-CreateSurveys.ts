a9f42402988363fe9599a8c4467b06f3aa466704
Marcelo Norberto <norbertomarcelo8@gmail.com>
2021-02-27 15:48:28 -0300

    Database connect

---
+++ src/database/migrations/1614451519703-CreateSurveys.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateSurveys1614451519703 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
