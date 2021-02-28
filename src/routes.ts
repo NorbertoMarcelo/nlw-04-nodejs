f4a6865ec620ff0ebbe4f93063ffd7aa154690d1
Marcelo Norberto <norbertomarcelo8@gmail.com>
2021-02-28 20:28:35 -0300

    Send emails

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+import { SendMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post('/users', userController.create);
 
 router.get('/surveys', surveysController.show);
 router.post('/surveys', surveysController.create);
+
+router.post('/sendMail', sendMailController.execute);
+
+router.get('/answers/:value', answerController.execute);
 
 export { router };
